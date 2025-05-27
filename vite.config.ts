b876a8b3e39aa4749be45cd7dad26cd7f4259168
Saqibworksehsan <saqibworksehsan@gmail.com>
2025-05-27 23:04:57 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/aawa.in/',
+  base: command === 'build' ? '/aawa.in/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
